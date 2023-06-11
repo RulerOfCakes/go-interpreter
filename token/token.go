f274aaa766aff09fe4764a0e2262d427b7b95431
RulerOfCakes <fragmentbombs@gmail.com>
2023-06-11 17:00:28 +0900

    feat: basic lexer token parsing

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1234567890
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// TODO: probably more appropriate to include filenames and line numbers here
+type Token struct {
+	Type    TokenType
+	Literal string
+}
